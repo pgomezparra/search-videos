0c3683e957addaf1be17d87f88db5280847aba97
Pedro Gomez <pandresgomezp@gmail.com>
2022-09-29 23:34:24 -0500

    Usando await para el fetch

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,20 @@
 	const commomProps = { maxCounter: 15 }
 </script>
 
+<style>
+	h1 {
+		font-size: 32px;
+	}
+	section {
+		align-items: center;
+		display: flex;
+		flex-direction: column; 
+	}
+</style>
 
-<Input/>
+<section>
+	<h1>Search Movies</h1>
+	<Input/>
+</section>
 <!-- <Counter initialCounter={2} {...commomProps} /> -->
 <Counter initialCounter={3} {...commomProps} />
--- src/Input.svelte
+++ src/Input.svelte
@@ -2,21 +2,16 @@
     import Movie from './Movie.svelte'
 
     let value = ''
-    let loading = false
     let response = []
 
     const handleInput = (event) => 
         value = event.target.value
 
-        $: if (value.length > 2) {
-            loading = true
-            fetch(`https://www.omdbapi.com/?s=${value}&apikey=422350ff`)
-                .then(res => res.json())
-                .then(apiResponse => {
-                    response = apiResponse.Search || []
-                    loading = false
-                })
-        }
+    $: if (value.length > 2) {
+        response = fetch(`https://www.omdbapi.com/?s=${value}&apikey=422350ff`)
+            .then(res => res.json())
+            .then(apiResponse =>  apiResponse.Search || [])
+    }
 </script>
 
 <input 
@@ -25,10 +20,10 @@
     on:input={handleInput} 
 />
 
-{#if loading}
+{#await response}
     <strong>Loading...</strong>
-{:else}
-    {#each response as {Title, Poster, Year}, index}
+{:then movies}
+    {#each movies as {Title, Poster, Year}}
         <Movie
             title={Title}
             poster={Poster}
@@ -37,4 +32,4 @@
     {:else}
         <strong>No hay resultados</strong>
     {/each}
-{/if}
+{/await}
