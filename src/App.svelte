a6c9dcec0707df68e614f125e646ca9a7a2611a1
Pedro Gomez <pandresgomezp@gmail.com>
2022-09-29 22:27:45 -0500

    Renderizado condicional

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,10 @@
 	import Counter from './Counter.svelte'
 	import Input from './Input.svelte'
 
-	const commomProps = { maxCounter: 5 }
+	const commomProps = { maxCounter: 15 }
 </script>
 
 
 <Input/>
-<!-- <Counter initialCounter={2} {...commomProps} />
-<Counter initialCounter={9} {...commomProps} /> -->
+<!-- <Counter initialCounter={2} {...commomProps} /> -->
+<Counter initialCounter={3} {...commomProps} />
--- src/Counter.svelte
+++ src/Counter.svelte
@@ -5,11 +5,11 @@
 	let contador = initialCounter;
 	
 	function handleClick() {
-		name = 'Fer'
 		contador++
 	}
 
 	$: isEvenOrOdd = contador % 2 === 0 ? 'Is Even' : 'Is Odd'
+	$: name = contador % 2 === 0 ? 'pedro' : 'fer'
 	$: if (contador > maxCounter) {
 			contador = maxCounter
 		}
@@ -18,10 +18,10 @@
 
 <main>
 	<button on:click={handleClick}>Incrementar</button>
-	<span>{contador}</span>
+	<p><span>{contador}</span></p>
+	<p><span>{isEvenOrOdd}</span></p>
 	<h1>Hola {name}!</h1>
 	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
-	<span>{isEvenOrOdd}</span>
 </main>
 
 <style>
@@ -32,10 +32,17 @@
 		margin: 0 auto;
 	}
 
-	h1, span {
+	h1 {
 		color: #175fe4;
 		text-transform: uppercase;
 		font-size: 4em;
+		font-weight: 100;
+	}
+
+	span {
+		color: #e47317;
+		text-transform: uppercase;
+		font-size: 2em;
 		font-weight: 100;
 	}
 
